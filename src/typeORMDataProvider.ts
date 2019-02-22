1cba6062079e9213a5497abb1e94990c914f9b04
Gints Polis <gints.polis@lattelecom.lv>
2019-02-23 00:35:37 +0200

    Better organisation

--- src/typeORMDataProvider.ts
+++ src/typeORMDataProvider.ts
@@ -1,41 +1,84 @@
 import * as vscode from 'vscode';
-import { getConnectionManager, EntityMetadata } from 'typeorm';
+import { getConnectionManager, EntityMetadata, Connection } from 'typeorm';
 import { openDatabase } from './openDatabase';
 
-export class TypeORMProvider implements vscode.TreeDataProvider<Model> {
-  onDidChangeTreeData?: vscode.Event<Model | null | undefined> | undefined;
-  getTreeItem(element: Model): vscode.TreeItem | Thenable<vscode.TreeItem> {
-    return element;
+interface TypeOrmObject {
+  readonly id: string;
+  readonly metadata?: any;
+  getChildren(): vscode.ProviderResult<TypeOrmObject[]>;
+  getTreeItem(): vscode.TreeItem | Thenable<vscode.TreeItem>;
+}
+
+export class TypeORMProvider implements vscode.TreeDataProvider<TypeOrmObject> {
+  onDidChangeTreeData?:
+    | vscode.Event<TypeOrmObject | null | undefined>
+    | undefined;
+  getTreeItem(
+    element: TypeOrmObject
+  ): vscode.TreeItem | Thenable<vscode.TreeItem> {
+    return element.getTreeItem();
   }
-  getChildren(element?: Model | undefined): vscode.ProviderResult<Model[]> {
+  getChildren(
+    element?: TypeOrmObject | undefined
+  ): vscode.ProviderResult<TypeOrmObject[]> {
     if (element) {
-      return [];
+      return element.getChildren();
     } else {
-      return this.getEntities();
+      return this.getConnections();
     }
   }
 
-  private async getEntities(): Promise<Model[]> {
+  private async getConnections(): Promise<TypeOrmObject[]> {
     const manager = getConnectionManager();
 
     if (manager.connections.length === 0) {
       await openDatabase();
     }
 
-    if (manager.connections[0]) {
-      const entities = manager.connections[0].entityMetadatas;
-      const items = entities.map((entity: EntityMetadata) => {
-        return new Model(entity.name);
-      });
-      return items;
-    } else {
-      return [new Model('DB not open')];
-    }
+    return manager.connections.map(connection => {
+      return new TypeOrmConnection(connection);
+    });
   }
 }
 
-export class Model extends vscode.TreeItem {
-  constructor(public readonly label: string) {
-    super(label);
+class TypeOrmConnection implements TypeOrmObject {
+  id: string;
+  metadata: Connection;
+
+  constructor(connection: Connection) {
+    this.metadata = connection;
+    this.id = connection.name;
+  }
+
+  getChildren(): TypeOrmObject[] {
+    const entities = this.metadata.entityMetadatas;
+    return entities.map(entity => {
+      return new TypeOrmEntity(entity);
+    });
+  }
+
+  getTreeItem(): vscode.TreeItem {
+    const treeItem = new vscode.TreeItem(this.id);
+    treeItem.collapsibleState = vscode.TreeItemCollapsibleState.Expanded;
+    return treeItem;
+  }
+}
+
+class TypeOrmEntity implements TypeOrmObject {
+  id: string;
+  metadata: EntityMetadata;
+
+  constructor(entity: EntityMetadata) {
+    this.metadata = entity;
+    this.id = entity.name;
+  }
+
+  getChildren(): TypeOrmObject[] {
+    return [];
+  }
+
+  getTreeItem(): vscode.TreeItem {
+    const treeItem = new vscode.TreeItem(this.id);
+    return treeItem;
   }
 }
